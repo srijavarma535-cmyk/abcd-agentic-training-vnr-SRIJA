cf82502a31ecd000af62fb1b04dc32af1ddb3c25
srijavarma535-cmyk <srijavarma535@gmail.com>
2026-04-10 08:08:46 +0530

    Delete Agentic AI Projects/ai_vc_due_diligence_v4_HTML/ai_vc_due_diligence directory

--- Agentic AI Projects/ai_vc_due_diligence_v4_HTML/ai_vc_due_diligence/Tokenization/CharacterLevel.py
+++
@@ -1,5 +0,0 @@
-def char_tokenizer(text):
-    return list(text)
-
-text = "NLP"
-print(char_tokenizer(text))
--- Agentic AI Projects/ai_vc_due_diligence_v4_HTML/ai_vc_due_diligence/Tokenization/SpaceAlgo.py
+++
@@ -1,9 +0,0 @@
-def whitespace_tokenizer(text):
-    tokens = text.split()
-    return tokens
-
-# Example
-text = "Tokenization is the first step in NLP."
-tokens = whitespace_tokenizer(text)
-
-print(tokens)
